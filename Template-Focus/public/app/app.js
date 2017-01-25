11858b194f7c91085d9aae0853a1fe33ef48e765
masterduck7 <luis.ramirez.12@sansano.usm.cl>
2017-01-25 17:39:53 -0300

    Show Offer implementado

--- Template-Focus/public/app/app.js
+++ Template-Focus/public/app/app.js
@@ -50,3 +50,50 @@ app.factory('Offer', function($resource) {
     }
   });
 });
+
+app.controller('MainCtrl', function($scope, srvShareData, $location) {
+  
+  $scope.dataToShare = [];
+  
+  $scope.shareMyData = function (myValue) {
+
+    $scope.dataToShare = myValue;
+    srvShareData.addData($scope.dataToShare);
+    
+  }
+});
+
+app.controller('MainCtrl2', function($scope, srvShareData) {
+  
+  $scope.sharedData = srvShareData.getData();
+
+});
+
+app.service('srvShareData', function($window) {
+        var KEY = 'App.SelectedValue';
+
+        var addData = function(newObj) {
+            var mydata = $window.sessionStorage.getItem(KEY);
+            if (mydata) {
+                mydata = JSON.parse(mydata);
+            } else {
+                mydata = [];
+            }
+            mydata = []
+            mydata.push(newObj);
+            $window.sessionStorage.setItem(KEY, JSON.stringify(mydata));
+        };
+
+        var getData = function(){
+            var mydata = $window.sessionStorage.getItem(KEY);
+            if (mydata) {
+                mydata = JSON.parse(mydata);
+            }
+            return mydata || [];
+        };
+
+        return {
+            addData: addData,
+            getData: getData
+        };
+    });

e9be13d9459854b778d72ab73c3b5284f30de160
masterduck7 <luis.ramirez.12@sansano.usm.cl>
2017-01-26 14:09:28 -0300

    Modificaciones admin.
    Falta implementar actualizar oferta

--- Template-Focus/public/app/app.js
+++ Template-Focus/public/app/app.js
@@ -97,3 +97,43 @@ app.service('srvShareData', function($window) {
             getData: getData
         };
     });
+
+app.controller('OffersCtrl', function($scope, Offer, ngProgress, toaster) {
+    
+    $scope.offer = new Offer();
+
+    var refresh = function() {
+      $scope.offers = Offer.query(); 
+      $scope.offer ="";
+    }
+    refresh();
+
+    $scope.add = function(offer) {
+      Offer.save(offer,function(offer){
+        refresh();
+      });
+      location.href = 'okpost.html';
+    };
+
+    $scope.update = function(offer) {
+      offer.$update(function(){
+        refresh();
+      });
+    };
+
+    $scope.remove = function(offer) {
+      offer.$delete(function(){
+        refresh();
+      });
+    };
+
+    $scope.edit = function(id) {
+      $scope.offer = Offer.get({ id: id });
+    };  
+
+    $scope.deselect = function() {
+      $scope.offer = "";
+    }
+
+}
+)

c5917d47f4ce5379cbd648022645ca817c24e102
Benjamin Miranda <benjamin.miranda.12@sansano.usm.cl>
2017-01-31 09:33:50 -0300

    gf

---
+++ Template-Focus/app/app.js
@@ -0,0 +1,139 @@
+var app = angular.module('myApp', ['ngResource', 'ngProgress', 'ngAnimate', 'toaster']);
+app.config(function ($httpProvider) {
+  $httpProvider.interceptors.push('myHttpInterceptor');
+});
+
+// Handle http server errors
+app.factory('myHttpInterceptor', function ($q,toaster) {
+    return {
+        responseError: function (response) {
+          console.log(response);
+          if(response.data){
+            if(response.data.message)
+            toaster.error("Error: ", response.data.message);
+            else
+            toaster.error("Error: ", response.data);
+          }
+          return $q.reject(response);
+        }
+    };
+});
+
+// Showing loading indicator on top while data is requested from database
+app.directive('loading',   ['$http', 'ngProgress', function ($http, ngProgress)
+{
+    return {
+        restrict: 'A',
+        link: function (scope, elm, attrs)
+        {
+            scope.isLoading = function () {
+                return $http.pendingRequests.length > 0;
+            };
+
+            scope.$watch(scope.isLoading, function (v)
+            {
+                if(v){
+                    ngProgress.start();
+                }else{
+                    ngProgress.complete();
+                }
+            });
+        }
+    };
+}]);
+
+// Create a resource factory to access offers table from database 
+app.factory('Offer', function($resource) {
+  return $resource('/api/offers/:id', { id: '@_id' }, {
+    update: { // We need to define this method manually as it is not provided with ng-resource
+      method: 'PUT'
+    }
+  });
+});
+
+app.controller('MainCtrl', function($scope, srvShareData, $location) {
+  
+  $scope.dataToShare = [];
+  
+  $scope.shareMyData = function (myValue) {
+
+    $scope.dataToShare = myValue;
+    srvShareData.addData($scope.dataToShare);
+    
+  }
+});
+
+app.controller('MainCtrl2', function($scope, srvShareData) {
+  
+  $scope.sharedData = srvShareData.getData();
+
+});
+
+app.service('srvShareData', function($window) {
+        var KEY = 'App.SelectedValue';
+
+        var addData = function(newObj) {
+            var mydata = $window.sessionStorage.getItem(KEY);
+            if (mydata) {
+                mydata = JSON.parse(mydata);
+            } else {
+                mydata = [];
+            }
+            mydata = []
+            mydata.push(newObj);
+            $window.sessionStorage.setItem(KEY, JSON.stringify(mydata));
+        };
+
+        var getData = function(){
+            var mydata = $window.sessionStorage.getItem(KEY);
+            if (mydata) {
+                mydata = JSON.parse(mydata);
+            }
+            return mydata || [];
+        };
+
+        return {
+            addData: addData,
+            getData: getData
+        };
+    });
+
+app.controller('OffersCtrl', function($scope, Offer, ngProgress, toaster) {
+    
+    $scope.offer = new Offer();
+
+    var refresh = function() {
+      $scope.offers = Offer.query(); 
+      $scope.offer ="";
+    }
+    refresh();
+
+    $scope.add = function(offer) {
+      Offer.save(offer,function(offer){
+        refresh();
+      });
+      location.href = 'okpost.html';
+    };
+
+    $scope.update = function(offer) {
+      offer.$update(function(){
+        refresh();
+      });
+    };
+
+    $scope.remove = function(offer) {
+      offer.$delete(function(){
+        refresh();
+      });
+    };
+
+    $scope.edit = function(id) {
+      $scope.offer = Offer.get({ id: id });
+    };  
+
+    $scope.deselect = function() {
+      $scope.offer = "";
+    }
+
+}
+)

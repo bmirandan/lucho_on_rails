d6c5c47760a5ce3bfefc56d4c0e04bbe8e48340a
Benjamin Miranda <benjamin.miranda.12@sansano.usm.cl>
2017-02-03 10:50:46 -0300

    v

--- master/DUC Client/source/js/controllers/logout.js
+++
@@ -1,9 +0,0 @@
-function LogoutCtrl($scope, $location, AuthenticationService) {
-    this.logout = function () {
-        AuthenticationService.ClearCredentials();
-        $location.path('/login');
-    };
-};
-
-angular.module('inspinia')
-	.controller('LogoutCtrl', ['$scope', '$location', 'AuthenticationService', LogoutCtrl] );
--- master/DUC Client/source/js/controllers/main.js
+++
@@ -1,7 +0,0 @@
-function MainCtrl($scope, $uibModal, $http, $q) {
-  
-};
-
-angular
-    .module('inspinia')
-    .controller('MainCtrl', ['$scope', '$uibModal', '$http', '$q', MainCtrl]);

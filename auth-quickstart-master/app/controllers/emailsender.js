2eff3d7b85019aa46ef61824019fd04a6ba340e1
Benjamin Miranda <benjamin.miranda.12@sansano.usm.cl>
2017-03-03 15:49:59 -0300

    cambios-vistas

--- auth-quickstart-master/app/controllers/emailsender.js
+++ auth-quickstart-master/app/controllers/emailsender.js
@@ -11,7 +11,7 @@ var smtpTransport = nodemailer.createTransport({
 
 smtpTransport.sendMail({  //email options
    from: "Sender Name <deskisio10@gmail.com>", // sender address.  Must be the same as authenticated user if using Gmail.
-   to: "Receiver Name <deskisio10@gmail.com>", // receiver
+   to: "Receiver Name <bmirandan@gmail.com>", // receiver
    subject: "necessita ayuda este "+ req.params.id, // subject
    text: " información importante " // body
 }, function(error, response){  //callback

580a198650ac079d6ff521cf286c303092f62fba
Benjamin Miranda <benjamin.miranda.12@sansano.usm.cl>
2017-03-26 22:06:40 -0300

    funciona-validacion-mas-no-registro
    Se utilizaron directivas para validar los campos, curiosamente pese a
    entregar datos correctos estos no son insertados en la base de datos.

--- auth-quickstart-master/styles/validator/app.js
+++ auth-quickstart-master/styles/validator/app.js
@@ -8,7 +8,7 @@ validationApp.controller('mainController', function($scope) {
 
     // check to make sure the form is completely valid
     if (isValid) {
-      alert('our form is amazing');
+      
     }
 
   };
